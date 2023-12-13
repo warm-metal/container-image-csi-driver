ce904639b420a533511df778610f70d27795a81b
vadasambar <suraj.bankar@acquia.com>
2023-12-13 17:24:17 +0530

    fix: ci/cd test failing - because the script assumes warm metal driver is deployed in `default` namespace (it is deployed in `kube-system` namespace)

--- test/integration/test-metrics.sh
+++ test/integration/test-metrics.sh
@@ -9,7 +9,7 @@ kubectl apply -f "${TestBase}/metrics-manifests/error-ephemeral-volume.yaml"
 lib::run_test_job "${TestBase}/metrics-manifests/no-error-ephemeral-volume.yaml"
 
 
-ip="$(kubectl get po -l=component=nodeplugin -ojsonpath='{.items[*].status.podIP}')"
+ip="$(kubectl get po -n kube-system -l=component=nodeplugin -ojsonpath='{.items[*].status.podIP}')"
 cat "${TestBase}/metrics-manifests/test.yaml" | sed "s|%IP|$ip|g" > "${TestBase}/metrics-manifests/rendered-test.yaml"
 
 lib::run_test_job "${TestBase}/metrics-manifests/rendered-test.yaml"
