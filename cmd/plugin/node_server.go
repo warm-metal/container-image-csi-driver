7c71b01ae52b0d6c3a1dae6fcfc1594c983423ab
vadasambar <suraj.bankar@acquia.com>
2023-12-19 19:07:30 +0530

    refactor: remove debug code

--- cmd/plugin/node_server.go
+++ cmd/plugin/node_server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"os"
 	"strings"
 
@@ -60,8 +59,6 @@ type NodeServer struct {
 
 func (n NodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (resp *csi.NodePublishVolumeResponse, err error) {
 	klog.Infof("mount request: %s", req.String())
-	b, _ := json.MarshalIndent(req, " ", " ")
-	klog.Infof("REQUEST: %+v", string(b))
 
 	if len(req.VolumeId) == 0 {
 		err = status.Error(codes.InvalidArgument, "VolumeId is missing")
