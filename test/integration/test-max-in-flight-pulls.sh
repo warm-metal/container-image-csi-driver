2a93b0d869ac89f4acf76a247db7eb5e6c520075
vadasambar <surajrbanakar@gmail.com>
2024-01-11 21:56:22 +0530

    test: fix CI tests failing for max-in-flight pulls - use the correct namespace (`kube-system` instead of `default`)

--- test/integration/test-max-in-flight-pulls.sh
+++ test/integration/test-max-in-flight-pulls.sh
@@ -8,16 +8,16 @@ TestBase=$(dirname "${BASH_SOURCE[0]}")
 
 echo "Restart the driver"
 
-kubectl delete po -l=app.kubernetes.io/name=warm-metal-csi-driver
-kubectlwait default -l=app.kubernetes.io/name=warm-metal-csi-driver
+kubectl delete po -l=app.kubernetes.io/name=warm-metal-csi-driver -nkube-system
+kubectlwait kube-system -l=app.kubernetes.io/name=warm-metal-csi-driver
 
 lib::run_test_job "${TestBase}/max-in-flight-pulls-manifests"
 
-warmmetalpod="$(kubectl get po -l=component=nodeplugin --no-headers | awk '{print $1}')"
-t1="$(kubectl logs $warmmetalpod csi-plugin | grep 'pull image "ubuntu:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
-t2="$(kubectl logs $warmmetalpod csi-plugin | grep 'image is ready for use: "ubuntu:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
-t3="$(kubectl logs $warmmetalpod csi-plugin | grep 'pull image "debian:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
-t4="$(kubectl logs $warmmetalpod csi-plugin | grep 'image is ready for use: "debian:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
+warmmetalpod="$(kubectl get po -nkube-system -l=component=nodeplugin --no-headers | awk '{print $1}')"
+t1="$(kubectl logs -nkube-system $warmmetalpod csi-plugin | grep 'pull image "ubuntu:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
+t2="$(kubectl logs -nkube-system $warmmetalpod csi-plugin | grep 'image is ready for use: "ubuntu:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
+t3="$(kubectl logs -nkube-system $warmmetalpod csi-plugin | grep 'pull image "debian:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
+t4="$(kubectl logs -nkube-system $warmmetalpod csi-plugin | grep 'image is ready for use: "debian:latest"' | awk '{print $2}' | xargs -I{} date -d '{}' +'%s')"
 
 kubectl delete --ignore-not-found -f "${TestBase}/compatible-manifests/pre-provisioned-pv.yaml"
 
