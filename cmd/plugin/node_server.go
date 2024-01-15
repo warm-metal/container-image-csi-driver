d402232189f170a180f0bdbc3183f0254585e280
vadasambar <surajrbanakar@gmail.com>
2024-01-15 11:02:57 +0530

    test: uncomment integration tests

--- cmd/plugin/node_server.go
+++ cmd/plugin/node_server.go
@@ -78,8 +78,6 @@ func (n NodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishV
 		return
 	}
 
-	//  comment to trigger CI
-
 	if _, isBlock := req.VolumeCapability.AccessType.(*csi.VolumeCapability_Block); isBlock {
 		err = status.Error(codes.InvalidArgument, "unable to mount as a block device")
 		return
