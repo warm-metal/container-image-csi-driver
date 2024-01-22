9498bb68fb4866e3fa28b15d4f46d880df3552f2
Mugdha Adhav <mugdha.adhav@gmail.com>
2024-01-22 20:30:30 +0530

    chore: update names of test images and its refs (#130)

--- test/integration/test-restart-runtime-crio.sh
+++ test/integration/test-restart-runtime-crio.sh
@@ -4,16 +4,16 @@ set -e
 source $(dirname "${BASH_SOURCE[0]}")/../../hack/lib/cluster.sh
 lib::start_cluster_crio ${K8S_VERSION}
 
-minikube -p csi-image-test node add
-minikube cache reload -p csi-image-test
+minikube -p container-image-csi-driver-test node add
+minikube cache reload -p container-image-csi-driver-test
 lib::install_driver
 
-minikube ssh -p csi-image-test -n csi-image-test-m02 -- sudo mkdir -p /mnt/vda1/var/lib/boot2docker/etc/crio
-minikube ssh -p csi-image-test -n csi-image-test-m02 -- sudo cp -rp /etc/crio/* /var/lib/boot2docker/etc/crio/
+minikube ssh -p container-image-csi-driver-test -n container-image-csi-driver-test-m02 -- sudo mkdir -p /mnt/vda1/var/lib/boot2docker/etc/crio
+minikube ssh -p container-image-csi-driver-test -n container-image-csi-driver-test-m02 -- sudo cp -rp /etc/crio/* /var/lib/boot2docker/etc/crio/
 
 source $(dirname "${BASH_SOURCE[0]}")/restart-runtime.sh 0
 
 lib::uninstall_driver
 echo "Destroying cluster"
-minikube delete -p csi-image-test
+minikube delete -p container-image-csi-driver-test
 set +e
