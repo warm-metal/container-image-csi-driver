406b3564ccdfb39245abbe6a5c2e75d22b3c2f43
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2024-02-29 18:39:31 +0100

    revert pull changes

--- pkg/remoteimage/pull.go
+++ pkg/remoteimage/pull.go
@@ -2,31 +2,17 @@ package remoteimage
 
 import (
 	"context"
-	"sync"
 
 	"github.com/containerd/containerd/reference/docker"
-	"golang.org/x/sync/semaphore"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	cri "k8s.io/cri-api/pkg/apis/runtime/v1"
-	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/credentialprovider"
 )
-
-type CurrentPull struct {
-	semaphore *semaphore.Weighted
-	err       error
-}
 
 type Puller interface {
 	Pull(context.Context) error
 	ImageSize(context.Context) int
 }
-
-var (
-	parallelLock  *sync.Mutex             = &sync.Mutex{}
-	parallelPull  map[string]*CurrentPull = make(map[string]*CurrentPull)
-	totalParallel *semaphore.Weighted     = semaphore.NewWeighted(10)
-)
 
 func NewPuller(imageSvc cri.ImageServiceClient, image docker.Named,
 	keyring credentialprovider.DockerKeyring) Puller {
@@ -65,43 +51,6 @@ func (p puller) Pull(ctx context.Context) (err error) {
 		return
 	}
 
-	parallelLock.Lock()
-	currentPullLock, ok := parallelPull[repo]
-	if !ok {
-		parallelPull[repo] = &CurrentPull{
-			semaphore: semaphore.NewWeighted(1),
-		}
-		currentPullLock = parallelPull[repo]
-	}
-	parallelLock.Unlock()
-
-	doingPull := currentPullLock.semaphore.TryAcquire(1)
-	if !doingPull {
-		klog.Infof("Pulling of image %s is already in progress wait until completed", repo)
-		currentPullLock.semaphore.Acquire(ctx, 1)
-	} else {
-		klog.Infof("Pulling of image %s not in progress, starting now", repo)
-	}
-
-	defer currentPullLock.semaphore.Release(1)
-
-	if !doingPull {
-		klog.Infof("Pulling of image %s is completed", repo)
-		return currentPullLock.err
-	}
-
-	err = totalParallel.Acquire(ctx, 1)
-	if err != nil {
-		return err
-	}
-	defer totalParallel.Release(1)
-
-	defer func() {
-		parallelLock.Lock()
-		delete(parallelPull, repo)
-		parallelLock.Unlock()
-	}()
-
 	var pullErrs []error
 	for _, cred := range creds {
 		auth := &cri.AuthConfig{
@@ -119,13 +68,11 @@ func (p puller) Pull(ctx context.Context) (err error) {
 		})
 
 		if err == nil {
-			klog.Info("Image pull completed successfully")
 			return
 		}
 
 		pullErrs = append(pullErrs, err)
 	}
 
-	currentPullLock.err = utilerrors.NewAggregate(pullErrs)
-	return err
+	return utilerrors.NewAggregate(pullErrs)
 }
