1d299aa62bcc9360bf51fd063c0bc26ac0c5e138
Mugdha Adhav <mugdha.adhav@gmail.com>
2024-08-29 12:50:05 +0530

    fix: avoid using builder cache in integration tests

--- hack/ci/build.sh
+++ hack/ci/build.sh
@@ -5,12 +5,21 @@ set -x
 VERSION=$(git rev-parse --short HEAD)
 IMG=docker.io/warmmetal/container-image-csi-driver:${VERSION}
 BUILDER=$(docker buildx ls | grep ci-builderx || true)
-[ "${BUILDER}" != "" ] || docker buildx create \
+
+if [ "${BUILDER}" != "" ]; then
+    docker buildx rm ci-builderx
+fi
+
+docker buildx create \
     --name ci-builderx --driver docker-container \
     --bootstrap \
     --driver-opt image=moby/buildkit:master,network=host
+
 docker buildx use ci-builderx
 docker buildx build -t ${IMG} -o "type=oci,dest=container-image-csi-driver.tar" .
 kind load image-archive container-image-csi-driver.tar -n kind-${GITHUB_RUN_ID}
 docker buildx build --target install-util -o "type=local,dest=_output/" .
+
+# Cleanup builder to avoid caching issues
+docker buildx rm ci-builderx
 set +e
